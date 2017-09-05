44140cdc004aeb3b78fab21186d41ee7f31fb1e1
Ricardo Dominguez <rdominguez@mrmilu.com>
2017-09-05 15:09:16 +0200

    + Added small animations

--- src/js/components/Subindexes/Subindex/Chart/ValueDroplet/index.jsx
+++ src/js/components/Subindexes/Subindex/Chart/ValueDroplet/index.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import './common.scss';
+
 export default class ValueDroplet extends PureComponent {
 
   static propTypes = {
@@ -12,7 +14,7 @@ export default class ValueDroplet extends PureComponent {
   render() {
     const { className, value, position } = this.props;
     return (
-      <svg className={ className } xmlns='http://www.w3.org/2000/svg' viewBox='0 0 37 46' style={ { left: `${ position }%` } }>
+      <svg className={ `bluetab-value-droplet ${ className ? className : '' }` } xmlns='http://www.w3.org/2000/svg' viewBox='0 0 37 46' style={ { left: `${ position }%` } }>
         <g fill='none' fillRule='evenodd'>
           <path d='M18.5.14C28.75.14 37 8.54 37 18.94c0 5.25-2.84 10.8-10.4 18.35l-8.1 8.23-8.25-8.25C2.85 29.6 0 24.04 0 18.92 0 8.52 8.25.12 18.5.12z' />
           <text fill='#FFF' fontFamily='Montserrat-Light, Montserrat' fontSize='1.4rem' fontWeight='300'>
