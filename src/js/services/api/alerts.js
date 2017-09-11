fc98c2867f85d40fa690f912fa20fd0d29279f97
Ricardo Dominguez <rdominguez@mrmilu.com>
2017-09-11 10:21:42 +0200

    + Small alert service fix

--- src/js/services/api/alerts.js
+++ src/js/services/api/alerts.js
@@ -2,7 +2,7 @@ import endpoint from 'services/api/config';
 
 export default class AlertsServices {
   static getAlerts(request) {
-    return fetch('js/data/alert.json')
+    return fetch('/data/alert.json')
       .then((response) => {
         if (response.status !== 200) {
           return {};
